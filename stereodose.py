d2c00ce6831fe95d769eabfb8fc984e7c784e8b8
abi <abi@grinser.de>
2015-04-01 23:07:17 +0200

    first steps

--- stereodose.py
+++ stereodose.py
@@ -2,17 +2,31 @@ from bs4 import BeautifulSoup, SoupStrainer
 from requests import get as http_get
 from json import loads as json_loads
 
-def get_streams():
-    html = BeautifulSoup(http_get('https://www.stereodose.com/playlist/weed/chill').content, parse_only=SoupStrainer('script'))
+
+BASE_URL = "https://www.stereodose.com"
+
+def get_streams(drug, mood):
+    ''' 
+        parse streams from created java script code 
+        tansform to python object by json
+    '''
+    html = BeautifulSoup(http_get(BASE_URL + '/playlist/' + drug + '/' + mood).content, parse_only=SoupStrainer('script'))
     stream_script = str(html('script')[1])
     script_parse = stream_script.split('[{', 1)[1].rsplit('}];', 1)[0]
     streamdata = '[{' + script_parse + '}]'
     jsondata = json_loads(streamdata)
+    streams = []
     for stream in jsondata:
-        print stream['soundurl']
+        streams.append(stream['soundurl'])
+
+    return streams
 
 def get_drugs():
-    html = BeautifulSoup(http_get('https://www.stereodose.com/').content, parse_only=SoupStrainer('a'))
+    '''
+        return list of possible drugs and moods 
+        to choose
+    '''
+    html = BeautifulSoup(http_get(BASE_URL).content, parse_only=SoupStrainer('a'))
     drugs = dict()
     for link in html.find_all('a', {'class':"filter_option"} ,href=True):
         linkdata = link.get('href')
@@ -26,5 +40,5 @@ def get_drugs():
 
     return drugs
 
-get_streams()
+print get_streams('weed','chill')
 print get_drugs()
