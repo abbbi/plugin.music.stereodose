a3c5ac8c88cf4d5a2c9187390947b1ca111292b3
ma <ma@sep.de>
2015-04-02 12:42:27 +0200

    first working version

--- stereodose.py
+++ stereodose.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup, SoupStrainer
-#import requests
 from json import loads as json_loads
-
+import re
 import xbmcgui
 import xbmcplugin
 import urllib
@@ -11,13 +10,14 @@ base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
 mode = args.get('mode', None)
-#
 xbmcplugin.setContent(addon_handle, 'movies')
-
+APPNAME = "Stereodose"
 BASE_URL = "https://www.stereodose.com"
 
 def get_streams(drug, mood):
     ''' 
+        ugly!
+
         parse streams from created java script code 
         tansform to python object by json
 
@@ -31,24 +31,45 @@ def get_streams(drug, mood):
     '''
     html = BeautifulSoup(urllib.urlopen(BASE_URL + '/playlist/' + drug + '/' + mood).read(), parse_only=SoupStrainer('script'))
     stream_script = str(html('script')[1])
+
+    '''
+        get list of streamcloud links
+    '''
     script_parse = stream_script.split('[{', 1)[1].rsplit('}];', 1)[0]
     streamdata = '[{' + script_parse + '}]'
     jsondata = json_loads(streamdata)
+
+    ''' get sc_client_id '''
+    pattern = re.compile("(.*)var.*?sc_client_id.*?=\s*(.*?);")
+    match = re.search(pattern, stream_script)
+
+    client_id=match.groups()[1].strip('"')
+
+    if not client_id:
+        errorout('Error: unable to discover streamcloud client ID')
+    
     streams = []
     for stream in jsondata:
-        streams.append(stream['soundurl'])
-
+        stream_id = stream['stream_id']
+        url = "https://api.soundcloud.com/tracks/"+stream_id+"/stream?client_id=" + client_id
+        streams.append(url)
+        
     if len(streams) <= 0:
-        return None
-
+        return None 
+        
     return streams
+
+def errorout(message):
+    dialog=xbmcgui.Dialog()
+    dialog.ok(APPNAME, message)
+    xbmcplugin.endOfDirectory(addon_handle)
+    xbmc.executebuiltin("XBMC.Container.Update(path,replace)")
 
 def get_drugs():
     '''
         return list of possible drugs and moods 
         to choose
     '''
-    print "gretdrugs"
     html = BeautifulSoup(urllib.urlopen(BASE_URL).read(), parse_only=SoupStrainer('a'))
     drugs = dict()
     for link in html.find_all('a', {'class':"filter_option"} ,href=True):
@@ -65,7 +86,6 @@ def get_drugs():
     if len(drugs) <= 0:
         return None
 
-    print "returndrugs"
     return drugs
 
 drugs = get_drugs()
@@ -97,5 +117,7 @@ if drugs != None:
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=stream, listitem=li)
  
     xbmcplugin.endOfDirectory(addon_handle)
+else:
+    errorout('Unable to get list of possible durgs to choose')
 
 xbmcplugin.endOfDirectory(addon_handle)
