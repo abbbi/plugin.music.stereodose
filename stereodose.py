5a2d518f877b62fab28766e6958989c34a36b5e8
ma <ma@sep.de>
2015-04-02 10:18:13 +0200

    second steps

--- stereodose.py
+++ stereodose.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup, SoupStrainer
-from requests import get as http_get
+#import requests
 from json import loads as json_loads
 
+import xbmcgui
+import xbmcplugin
+import urllib
+import urlparse
+
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+mode = args.get('mode', None)
+#
+xbmcplugin.setContent(addon_handle, 'movies')
 
 BASE_URL = "https://www.stereodose.com"
 
@@ -10,7 +21,7 @@ def get_streams(drug, mood):
         parse streams from created java script code 
         tansform to python object by json
     '''
-    html = BeautifulSoup(http_get(BASE_URL + '/playlist/' + drug + '/' + mood).content, parse_only=SoupStrainer('script'))
+    html = BeautifulSoup(urllib.urlopen(BASE_URL + '/playlist/' + drug + '/' + mood).read(), parse_only=SoupStrainer('script'))
     stream_script = str(html('script')[1])
     script_parse = stream_script.split('[{', 1)[1].rsplit('}];', 1)[0]
     streamdata = '[{' + script_parse + '}]'
@@ -29,9 +40,11 @@ def get_drugs():
         return list of possible drugs and moods 
         to choose
     '''
-    html = BeautifulSoup(http_get(BASE_URL).content, parse_only=SoupStrainer('a'))
+    print "gretdrugs"
+    html = BeautifulSoup(urllib.urlopen(BASE_URL).read(), parse_only=SoupStrainer('a'))
     drugs = dict()
     for link in html.find_all('a', {'class':"filter_option"} ,href=True):
+        print "loop"
         linkdata = link.get('href')
         ''' only return official playlists for the moment '''
         if "playlist" in linkdata:
@@ -44,12 +57,37 @@ def get_drugs():
     if len(drugs) <= 0:
         return None
 
+    print "returndrugs"
     return drugs
 
-drugs =  get_drugs()
+drugs = get_drugs()
 
-for drug in drugs:
-    print drug
-    for mood in drugs[drug]:
-        print mood
-        print get_streams(drug,mood)
+if drugs != None:
+    dialog = xbmcgui.Dialog()
+    drug_list = drugs.keys()
+    drug_choice = dialog.select('Select drug', drug_list)
+    del dialog
+
+    dialog = xbmcgui.Dialog()
+    mood_list = drugs[drug_list[drug_choice]]
+    print drug_choice
+    print mood_list
+    mood_choice = dialog.select('Select mood', mood_list)
+    del dialog
+    
+    stream_mood = mood_list[mood_choice]
+    stream_drug = drug_list[drug_choice]
+
+    print "playlist/" + stream_drug + "/" + stream_mood
+
+    streams = get_streams(stream_drug, stream_mood)
+
+    l = 0
+    for stream in streams:
+        li = xbmcgui.ListItem("Stream ID: " + str(l), iconImage='DefaultVideo.png')
+        l=l+1
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=stream, listitem=li)
+ 
+    xbmcplugin.endOfDirectory(addon_handle)
+
+xbmcplugin.endOfDirectory(addon_handle)
